68438d84b56ac41dd1903d6ec5f7afbd420b8593
abhinaya-aryal <aryalabhinaya343@gmail.com>
2022-09-16 10:32:32 +0545

    react-icons-added-typescript-removed

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="text-1.6 w-max mx-auto my-2 bg-green-500/75 p-0.4 rounded-lg border-2 border-solid border-black cursor-pointer">
+      Project Template By Abhinaya Aryal
+    </div>
+  );
+};
+
+export default Home;
